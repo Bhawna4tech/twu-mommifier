62e78a38ecce2ae4e4fa6b3df208bf5f8206247d
Bhawna Kumari <kbhawna@thoughtworks.com>
2017-06-29 00:07:56 +0530

    [Bhawna] Mommify String having more than 30% of vowels

--- src/mommy/Mommy.java
+++ src/mommy/Mommy.java
@@ -8,20 +8,34 @@ class Mommy {
     }
 
     String mommify() {
-        String mommifiedString = "";
-        int noOfVowels = 0;
-        int i = 0;
-        while (i < inputString.length()) {
-            if (isCharacterAVowel(inputString.charAt(i))) {
-                noOfVowels++;
-            } else
-                noOfVowels = 0;
-            if (noOfVowels < 2) {
-                mommifiedString = mommifiedString.concat(mommifySingleCharacter(inputString.charAt(i)));
+        if (percentOfVowelsIsMoreThan30()) {
+            String mommifiedString = "";
+            int noOfVowels = 0;
+            int i = 0;
+            while (i < inputString.length()) {
+                if (isCharacterAVowel(inputString.charAt(i))) {
+                    noOfVowels++;
+                } else
+                    noOfVowels = 0;
+                if (noOfVowels < 2) {
+                    mommifiedString = mommifiedString.concat(mommifySingleCharacter(inputString.charAt(i)));
+                }
+                i++;
             }
-            i++;
-        }
-        return mommifiedString;
+            return mommifiedString;
+        } else return inputString;
+    }
+
+    private boolean percentOfVowelsIsMoreThan30() {
+        if (inputString.length() > 2) {
+            double numberOfVowels = 0;
+            for (int i = 0; i < inputString.length(); i++) {
+                if (isCharacterAVowel(inputString.charAt(i))) {
+                    numberOfVowels++;
+                }
+            }
+            return ((numberOfVowels / inputString.length()) * 100) > 30.0;
+        } else return true;
     }
 
     private String mommifySingleCharacter(char singleCharacter) {
