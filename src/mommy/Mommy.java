cf03e0894c360a853c8a6e4a4a35b532c02f0dbf
Bhawna Kumari <kbhawna@thoughtworks.com>
2017-06-28 09:43:51 +0530

    [Bhawna | Laura] Do not mommify non vowel and an empty string

---
+++ src/mommy/Mommy.java
@@ -0,0 +1,16 @@
+package mommy;
+
+/**
+ * Created by bhawnakumari on 6/28/17.
+ */
+public class Mommy {
+    private String inputString;
+
+    Mommy(String inputString) {
+        this.inputString = inputString;
+    }
+
+    String mommify() {
+        return this.inputString;
+    }
+}
--- test/mommy/MommyTest.java
+++ test/mommy/MommyTest.java
@@ -2,12 +2,22 @@ package mommy;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by bhawnakumari on 6/28/17.
  */
 public class MommyTest {
 
     @Test
-    public void test(){}
+    public void shouldNotMommifyEmptyString(){
+        Mommy nullMommy = new Mommy("");
+        assertEquals("",nullMommy.mommify());
+    }
 
+    @Test
+    public void shouldNotMommifySingleNonVowel(){
+        Mommy singleNonVowel = new Mommy("x");
+        assertEquals("x", singleNonVowel.mommify());
+    }
 }
