d62245d9f98cfd45bacf2224c1f145c0a7ae1ac6
Bhawna Kumari <kbhawna@thoughtworks.com>
2017-07-04 21:13:37 +0530

    [Bhawna] Refactored by introducing constants, simplified expressions

--- src/mommy/Mommy.java
+++ src/mommy/Mommy.java
@@ -1,6 +1,10 @@
 package mommy;
 
 class Mommy {
+    public static final String MOMMY = "mommy";
+    public static final String EMPTY_STRING = "";
+    public static final double PERCENT_CONDITION_TO_MOMMIFY = 30.00;
+    public static final int HUNDRED = 100;
     private String inputString;
 
     Mommy(String inputString) {
@@ -8,7 +12,9 @@ class Mommy {
     }
 
     String mommify() {
-        if (percentOfVowelsIsMoreThan30()) {
+        if (!percentOfVowelsIsMoreThan30()) {
+            return inputString;
+        } else {
             String mommifiedString = "";
             int noOfVowels = 0;
             int i = 0;
@@ -23,26 +29,21 @@ class Mommy {
                 i++;
             }
             return mommifiedString;
-        } else return inputString;
+        }
     }
 
     private boolean percentOfVowelsIsMoreThan30() {
-        if (inputString.length() > 2) {
-            double numberOfVowels = 0;
-            for (int i = 0; i < inputString.length(); i++) {
-                if (isCharacterAVowel(inputString.charAt(i))) {
-                    numberOfVowels++;
-                }
+        double numberOfVowels = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            if (isCharacterAVowel(inputString.charAt(i))) {
+                numberOfVowels++;
             }
-            return ((numberOfVowels / inputString.length()) * 100) > 30.00;
-        } else return true;
+        }
+        return ((numberOfVowels / inputString.length()) * HUNDRED) > PERCENT_CONDITION_TO_MOMMIFY;
     }
 
     private String mommifySingleCharacter(char singleCharacter) {
-        if (isCharacterAVowel(singleCharacter)) {
-            return "mommy";
-        }
-        return singleCharacter + "";
+        return isCharacterAVowel(singleCharacter) ? MOMMY : singleCharacter + EMPTY_STRING;
     }
 
     private boolean isCharacterAVowel(char singleCharacter) {
